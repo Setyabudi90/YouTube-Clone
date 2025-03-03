e16106513119b8faed30b3839a440df72cda13c2
Inggrit Setya Budi <115447070+Setyabudi90@users.noreply.github.com>
2025-03-03 17:42:51 +0700

    Update postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,13 +1,8 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    "postcss-import": {},
-    "tailwindcss/nesting": {},
     tailwindcss: {},
     autoprefixer: {},
-    ...(process.env.NODE_ENV === "production"
-      ? { "./postcss-obfuscator": {} }
-      : {}),
   },
 };
 
